dcfc9b8da8146b5a577f394a69b9fd919a41b50a
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-05-16 01:10:29 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/codersoad/covid19/covid19_tracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.codersoad.covid19.covid19_tracker
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
