914318fa92708e586dc65313f5119f9501e55512
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-05-20 22:26:15 +0600

    DAY-2

---
+++ lib/Networking/Network.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+class Network{
+  final String url;
+  final api_key="";
+  Network(this.url);
+
+}
---
+++ lib/Screens/Loading_Screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class loading_screen extends StatefulWidget {
+  @override
+  _loading_screenState createState() => _loading_screenState();
+}
+
+class _loading_screenState extends State<loading_screen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCircle(
+          color: Colors.blueGrey,
+          size: 50,
+        ),
+      ),
+
+    );
+  }
+}
+
