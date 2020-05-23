64e3a7e0ec1097c5e18be82989aedb0f9b525571
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-05-23 15:11:42 +0600

    Build APK

---
+++ android/app/src/profile/gen/com/codersoad/covid19/covid19_tracker/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.codersoad.covid19.covid19_tracker;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
